1f8637acc797917637b1a319886a99c38e7a7428
NgurahNova <ngurahnovabali@gmail.com>
2025-05-22 22:48:31 +0800

    third Commit

--- config-head.h
+++ config-head.h
@@ -12,22 +12,14 @@
 const char* ssid = "Sandira";
 const char* password = "sandiraagro123";
 
-// NTP Server for time synchronization
-// const char* ntpServer1 = "id.pool.ntp.org";
-// const char* ntpServer2 = "pool.ntp.org";
-// const char* ntpServer3 = "time.google.com";
-// const long gmtOffset_sec = 8*3600;  // GMT+7 for Jakarta
-// const int daylightOffset_sec = 0;
-
-
 // Pin Definitions
 const int BUZZER_PIN = 25;
 const int PUMP_RELAY = 26;
 const int ONE_WIRE_BUS = 14;
 
-// Kalibrasi Sensor
+
 // ajust to input value soil sensor
 const int DRY_VALUE = 4095;    
 const int WET_VALUE = 1700;    
 const int MOISTURE_THRESHOLD = 55;
-#endif // CONFIG_HEAD_H
+#endif 
